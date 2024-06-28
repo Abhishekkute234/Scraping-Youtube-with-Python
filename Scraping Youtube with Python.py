ce61e52d6b84d105427f58688a954546017e36a9
Abhishek Kute <148034480+Abhishekkute234@users.noreply.github.com>
2024-06-28 15:20:57 +0530

    python file added

---
+++ Scraping Youtube with Python.py
@@ -0,0 +1,22 @@
+# Scraping Youtube with Python
+from pytube import YouTube
+
+link = input("Enter Link of Youtube Video: ")
+yt = YouTube(link)
+
+# To print title
+print("Title :", yt.title)
+# To get number of views
+print("Views :", yt.views)
+# To get the length of video
+print("Duration :", yt.length)
+# To get description
+print("Description :", yt.description)
+# To get ratings
+print("Ratings :", yt.rating)
+
+stream = yt.streams.filter(progressive=True).order_by('resolution').desc().first()
+
+# Download the video
+stream.download()
+print("Download completed!")
